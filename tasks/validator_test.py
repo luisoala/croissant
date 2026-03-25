fac8b5b4301d745d01f9ef0f086e27cafa3e9717
Leonardo Martins Bianco <leonardo@rhl.com.br>
2026-03-25 15:46:03 +0100

    fix - TaskSolution should have no Spec attributes. Added direct Task example to test suite.

--- tasks/validator_test.py
+++ tasks/validator_test.py
@@ -10,12 +10,12 @@ class TestCroissantTasksValidator(unittest.TestCase):
 
   def test_invalid_problem(self):
     conforms, text = validate_data("testdata/invalid_problem.jsonld")
-    self.assertFalse(conforms, "Problem with concrete output should fail.")
+    self.assertFalse(conforms, "Problem with no specs should fail.")
     self.assertIn(
-        "A TaskProblem must specify its expected output using a generic"
-        " OutputSpec.",
+        "A TaskProblem must have at least one property (input, output, or implementation) that is a spec class",
         text,
     )
+
 
   def test_valid_solution(self):
     conforms, _ = validate_data("testdata/valid_solution.jsonld")
@@ -31,5 +31,19 @@ class TestCroissantTasksValidator(unittest.TestCase):
     )
 
 
+  def test_direct_task(self):
+    conforms, _ = validate_data("testdata/direct_task.jsonld")
+    self.assertTrue(conforms, "Direct Task with concrete values should pass.")
+
+  def test_invalid_solution_with_spec(self):
+    conforms, text = validate_data("testdata/invalid_solution_with_spec.jsonld")
+    self.assertFalse(conforms, "Solution with spec should fail.")
+    self.assertIn(
+        "A TaskSolution cannot have an OutputSpec as output.",
+        text,
+    )
+
+
 if __name__ == "__main__":
   unittest.main()
+
